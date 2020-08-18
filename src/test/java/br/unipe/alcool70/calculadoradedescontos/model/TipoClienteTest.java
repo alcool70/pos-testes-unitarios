4a14b58929564c5a3ef0eb28ec52bbd046071b3c
Diego Rustnnes Bandeira <diego.rustnnes@conductor.com.br>
2020-08-18 15:02:51 -0300

    test: format and insert @DisplayName for test methods

--- src/test/java/br/unipe/alcool70/calculadoradedescontos/model/TipoClienteTest.java
+++ src/test/java/br/unipe/alcool70/calculadoradedescontos/model/TipoClienteTest.java
@@ -2,7 +2,7 @@ package br.unipe.alcool70.calculadoradedescontos.model;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TipoClienteTest {
     /*Inicializacao do Teste*/
@@ -12,13 +12,15 @@ class TipoClienteTest {
     Integer qtdIAPartirDe1000;
 
     @BeforeAll
-    public static void beforeAll(){
+    public static void beforeAll() {
         System.out.println("Iniciando Testes");
     }
+
     @AfterAll
-    public static void AfterAll(){
+    public static void AfterAll() {
         System.out.println("Fim dos Testes");
     }
+
     @BeforeEach
     void setUp() {
         this.qtdInferiorA100 = 99;
@@ -30,8 +32,10 @@ class TipoClienteTest {
     void tearDown() {
         this.driver = null;
     }
+
     @Test
-    @Tags({@Tag("All"),@Tag("ClienteA")})
+    @Tags({@Tag("All"), @Tag("ClienteA")})
+    @DisplayName("Fator Desconto será 0.9 quando Cliente for tipo A e Quantidade < 100")
     public void testGetFatorDescontoQuandoClienteAQuantidadeIferiorA100EntaoFatorDescontoSera09() {
         this.driver = TipoCliente.A;
         //Teste Procedure
@@ -39,8 +43,10 @@ class TipoClienteTest {
         //Verificacao
         assertEquals(0.90, resultado);
     }
+
     @Test
-    @Tags({@Tag("All"),@Tag("ClienteA")})
+    @Tags({@Tag("All"), @Tag("ClienteA")})
+    @DisplayName("Fator Desconto será 0.95 quando Cliente for tipo A e Quantidade < 1000")
     public void testGetFatorDescontoQuandoClienteAQuantidadeIferiorA1000EntaoFatorDescontoSera095() {
         this.driver = TipoCliente.A;
         //Teste Procedure
@@ -48,8 +54,10 @@ class TipoClienteTest {
         //Verificacao
         assertEquals(0.95, resultado);
     }
+
     @Test
-    @Tags({@Tag("All"),@Tag("ClienteA")})
+    @Tags({@Tag("All"), @Tag("ClienteA")})
+    @DisplayName("Fator Desconto será 1.0 quando Cliente for tipo A e Quantidade >= 1000")
     public void testGetFatorDescontoQuandoClienteAQuantidadeAPartirDe1000EntaoFatorDescontoSera100() {
         this.driver = TipoCliente.A;
         //Teste Procedure
@@ -57,8 +65,10 @@ class TipoClienteTest {
         //Verificacao
         assertEquals(1.00, resultado);
     }
+
     @Test
-    @Tags({@Tag("All"),@Tag("ClienteB")})
+    @Tags({@Tag("All"), @Tag("ClienteB")})
+    @DisplayName("Fator Desconto será 0.85 quando Cliente for tipo B e Quantidade < 100")
     public void testGetFatorDescontoQuandoClienteBQuantidadeIferiorA100EntaoFatorDescontoSera085() {
         this.driver = TipoCliente.B;
         //Teste Procedure
@@ -66,8 +76,10 @@ class TipoClienteTest {
         //Verificacao
         assertEquals(0.85, resultado);
     }
+
     @Test
-    @Tags({@Tag("All"),@Tag("ClienteB")})
+    @Tags({@Tag("All"), @Tag("ClienteB")})
+    @DisplayName("Fator Desconto será 0.9 quando Cliente for tipo B e Quantidade < 1000")
     public void testGetFatorDescontoQuandoClienteBQuantidadeIferiorA1000EntaoFatorDescontoSera090() {
         this.driver = TipoCliente.B;
         //Teste Procedure
@@ -75,8 +87,10 @@ class TipoClienteTest {
         //Verificacao
         assertEquals(0.90, resultado);
     }
+
     @Test
-    @Tags({@Tag("All"),@Tag("ClienteB")})
+    @Tags({@Tag("All"), @Tag("ClienteB")})
+    @DisplayName("Fator Desconto será 0.95 quando Cliente for tipo B e Quantidade >= 1000")
     public void testGetFatorDescontoQuandoClienteBQuantidadeAPartirDe1000EntaoFatorDescontoSera095() {
         this.driver = TipoCliente.B;
         //Teste Procedure
@@ -84,8 +98,10 @@ class TipoClienteTest {
         //Verificacao
         assertEquals(0.95, resultado);
     }
+
     @Test
-    @Tags({@Tag("All"),@Tag("ClienteC")})
+    @Tags({@Tag("All"), @Tag("ClienteC")})
+    @DisplayName("Fator Desconto será 0.80 quando Cliente for tipo C e Quantidade < 100")
     public void testGetFatorDescontoQuandoClienteCQuantidadeIferiorA100EntaoFatorDescontoSera080() {
         this.driver = TipoCliente.C;
         //Teste Procedure
@@ -93,8 +109,10 @@ class TipoClienteTest {
         //Verificacao
         assertEquals(0.80, resultado);
     }
+
     @Test
-    @Tags({@Tag("All"),@Tag("ClienteC")})
+    @Tags({@Tag("All"), @Tag("ClienteC")})
+    @DisplayName("Fator Desconto será 0.85 quando Cliente for tipo C e Quantidade < 1000")
     public void testGetFatorDescontoQuandoClienteCQuantidadeIferiorA1000EntaoFatorDescontoSera085() {
         this.driver = TipoCliente.C;
         //Teste Procedure
@@ -102,8 +120,10 @@ class TipoClienteTest {
         //Verificacao
         assertEquals(0.85, resultado);
     }
+
     @Test
-    @Tags({@Tag("All"),@Tag("ClienteC")})
+    @Tags({@Tag("All"), @Tag("ClienteC")})
+    @DisplayName("Fator Desconto será 0.90 quando Cliente é tipo C e Quantidade >= 1000")
     public void testGetFatorDescontoQuandoClienteCQuantidadeAPartirDe1000EntaoFatorDescontoSera090() {
         this.driver = TipoCliente.C;
         //Teste Procedure
