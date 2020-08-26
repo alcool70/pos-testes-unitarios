8036af3b9e2d8a7e294a13884f9ecbf9244ed189
Diego Rustnnes Bandeira <diego.rustnnes@conductor.com.br>
2020-08-26 00:23:28 -0300

    test: testing Item.getValorComDesconto from ItemService returned items

--- src/test/java/br/unipe/alcool70/calculadoradedescontos/service/ItemServiceMockTest.java
+++ src/test/java/br/unipe/alcool70/calculadoradedescontos/service/ItemServiceMockTest.java
@@ -29,6 +29,7 @@ public class ItemServiceMockTest {
     Integer totalDeItens = 10;
     Long idItemValido = 1L;
     Long idItemInvalido = 0L;
+    double tolerance = 0.01;
 
     @BeforeEach
     public void setUp() {
@@ -44,7 +45,7 @@ public class ItemServiceMockTest {
     @DisplayName("Quando tenho 10 itens então 10 itens são carregados")
     public void findAllQuandoTenho10ItensEntao10ItensSaoCarregados() {
         Mockito.when(this.mock.getAll()).thenReturn(
-                generateItems(this.totalDeItens, new Random().nextInt(3000), "")
+                generateItems(this.totalDeItens, new Random().nextInt(3000), -1, "")
         );
         //Procedimento
         Collection<Item> resultados = this.driver.findAll();
@@ -54,9 +55,9 @@ public class ItemServiceMockTest {
     }
 
     @Test
-    @DisplayName("Quando não tenho itens então nenhum iten é carregado")
+    @DisplayName("Quando não tenho itens então nenhum item é carregado")
     public void testFindAllQuandoNenhumItemEntao0ItemsSaoCarregados() {
-        Mockito.when(this.mock.getAll()).thenReturn(generateItems(0, 0, ""));
+        Mockito.when(this.mock.getAll()).thenReturn(generateItems(0, 0, -1,""));
         //Procedimento
         Collection<Item> resultados = this.driver.findAll();
         //Verificacao
@@ -77,7 +78,7 @@ public class ItemServiceMockTest {
     @DisplayName("Quando tenho 1 item válido então 1 item é encontrado")
     public void testFindByIdQuandoIdItemValidoEntao1ItemRetornado() {
         Mockito.when(this.mock.get(1L)).thenReturn(Optional.of(
-                generateItems(1, new Random().nextInt(3000), "")
+                generateItems(1, new Random().nextInt(3000), -1,"")
                         .iterator().next()
         ));
         //Procedimento
@@ -85,5 +86,140 @@ public class ItemServiceMockTest {
         //Verificacao
         assertTrue(resultado.isPresent());
         assertNotNull(resultado.get());
+    }
+
+    @Test
+    @DisplayName("Para 1 item válido com Cliente A, qtde 1 e valor do produto 123.45 "+
+            "então 1 item é encontrado com Valor com desconto 111.10±0.01")
+    public void testClienteAQtd1VlrThen1ItemVlr() {
+        Mockito.when(this.mock.get(1L)).thenReturn(Optional.of(
+                generateItems(1, 1, 123.45,"A").iterator().next()
+        ));
+        //Procedimento
+        Optional<Item> resultado = this.driver.findById(this.idItemValido);
+        //Verificacao
+        assertTrue(resultado.isPresent());
+        assertNotNull(resultado.get());
+        assertEquals(111.1, resultado.get().getValorComDesconto(), tolerance);
+    }
+
+    @Test
+    @DisplayName("Para 1 item válido com Cliente B, qtde 1 e valor do produto 123.45 "+
+            "então 1 item é encontrado com Valor com desconto 104.93±0.01")
+    public void testClienteBQtd1VlrThen1ItemVlr() {
+        Mockito.when(this.mock.get(1L)).thenReturn(Optional.of(
+                generateItems(1, 1, 123.45,"B").iterator().next()
+        ));
+        //Procedimento
+        Optional<Item> resultado = this.driver.findById(this.idItemValido);
+        //Verificacao
+        assertTrue(resultado.isPresent());
+        assertNotNull(resultado.get());
+        assertEquals(104.93, resultado.get().getValorComDesconto(), tolerance);
+    }
+
+    @Test
+    @DisplayName("Para 1 item válido com Cliente C, qtde 1 e valor do produto 123.45 "+
+            "então 1 item é encontrado com Valor com desconto 98.76±0.01")
+    public void testClienteCQtd1VlrThen1ItemVlr() {
+        Mockito.when(this.mock.get(1L)).thenReturn(Optional.of(
+                generateItems(1, 1, 123.45,"C").iterator().next()
+        ));
+        //Procedimento
+        Optional<Item> resultado = this.driver.findById(this.idItemValido);
+        //Verificacao
+        assertTrue(resultado.isPresent());
+        assertNotNull(resultado.get());
+        assertEquals(98.76, resultado.get().getValorComDesconto(), tolerance);
+    }
+
+    @Test
+    @DisplayName("Para 1 item válido com Cliente A, qtde 100 e valor do produto 123.45 "+
+            "então 1 item é encontrado com Valor com desconto 117.27±0.01")
+    public void testClienteAQtd100VlrThen1ItemVlr() {
+        Mockito.when(this.mock.get(1L)).thenReturn(Optional.of(
+                generateItems(1, 100, 123.45,"A").iterator().next()
+        ));
+        //Procedimento
+        Optional<Item> resultado = this.driver.findById(this.idItemValido);
+        //Verificacao
+        assertTrue(resultado.isPresent());
+        assertNotNull(resultado.get());
+        assertEquals(117.27, resultado.get().getValorComDesconto(), tolerance);
+    }
+
+    @Test
+    @DisplayName("Para 1 item válido com Cliente B, qtde 100 e valor do produto 123.45 "+
+            "então 1 item é encontrado com Valor com desconto 111.10±0.01")
+    public void testClienteBQtd100VlrThen1ItemVlr() {
+        Mockito.when(this.mock.get(1L)).thenReturn(Optional.of(
+                generateItems(1, 100, 123.45,"B").iterator().next()
+        ));
+        //Procedimento
+        Optional<Item> resultado = this.driver.findById(this.idItemValido);
+        //Verificacao
+        assertTrue(resultado.isPresent());
+        assertNotNull(resultado.get());
+        assertEquals(111.1, resultado.get().getValorComDesconto(), tolerance);
+    }
+
+    @Test
+    @DisplayName("Para 1 item válido com Cliente C, qtde 100 e valor do produto 123.45 "+
+            "então 1 item é encontrado com Valor com desconto 104.93±0.01")
+    public void testClienteCQtd100VlrThen1ItemVlr() {
+        Mockito.when(this.mock.get(1L)).thenReturn(Optional.of(
+                generateItems(1, 100, 123.45,"C").iterator().next()
+        ));
+        //Procedimento
+        Optional<Item> resultado = this.driver.findById(this.idItemValido);
+        //Verificacao
+        assertTrue(resultado.isPresent());
+        assertNotNull(resultado.get());
+        assertEquals(104.93, resultado.get().getValorComDesconto(), tolerance);
+    }
+
+    @Test
+    @DisplayName("Para 1 item válido com Cliente A, qtde 100 e valor do produto 123.45 "+
+            "então 1 item é encontrado com Valor com desconto 123.45±0.01")
+    public void testClienteAQtd1000VlrThen1ItemVlr() {
+        Mockito.when(this.mock.get(1L)).thenReturn(Optional.of(
+                generateItems(1, 1000, 123.45,"A").iterator().next()
+        ));
+        //Procedimento
+        Optional<Item> resultado = this.driver.findById(this.idItemValido);
+        //Verificacao
+        assertTrue(resultado.isPresent());
+        assertNotNull(resultado.get());
+        assertEquals(123.45, resultado.get().getValorComDesconto(), tolerance);
+    }
+
+    @Test
+    @DisplayName("Para 1 item válido com Cliente B, qtde 1000 e valor do produto 123.45 "+
+            "então 1 item é encontrado com Valor com desconto 117.27±0.01")
+    public void testClienteBQtd1000VlrThen1ItemVlr() {
+        Mockito.when(this.mock.get(1L)).thenReturn(Optional.of(
+                generateItems(1, 1000, 123.45,"B").iterator().next()
+        ));
+        //Procedimento
+        Optional<Item> resultado = this.driver.findById(this.idItemValido);
+        //Verificacao
+        assertTrue(resultado.isPresent());
+        assertNotNull(resultado.get());
+        assertEquals(117.27, resultado.get().getValorComDesconto(), tolerance);
+    }
+
+    @Test
+    @DisplayName("Para 1 item válido com Cliente C, qtde 1000 e valor do produto 123.45 "+
+            "então 1 item é encontrado com Valor com desconto 111.10±0.01")
+    public void testClienteCQtd1000VlrThen1ItemVlr() {
+        Mockito.when(this.mock.get(1L)).thenReturn(Optional.of(
+                generateItems(1, 1000, 123.45,"C").iterator().next()
+        ));
+        //Procedimento
+        Optional<Item> resultado = this.driver.findById(this.idItemValido);
+        //Verificacao
+        assertTrue(resultado.isPresent());
+        assertNotNull(resultado.get());
+        assertEquals(111.11, resultado.get().getValorComDesconto(), tolerance);
     }
 }
