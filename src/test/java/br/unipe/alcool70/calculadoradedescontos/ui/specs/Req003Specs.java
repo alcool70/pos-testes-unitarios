5afba9c18c1380ce3970491ce760cb89493a5201
Diego Rustnnes Bandeira <diego.rustnnes@conductor.com.br>
2020-09-02 01:07:27 -0300

    feat: Contact page tests

---
+++ src/test/java/br/unipe/alcool70/calculadoradedescontos/ui/specs/Req003Specs.java
@@ -0,0 +1,37 @@
+package br.unipe.alcool70.calculadoradedescontos.ui.specs;
+
+import br.unipe.alcool70.calculadoradedescontos.ui.steps.Req003Steps;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+import net.thucydides.core.annotations.Steps;
+
+import java.util.Map;
+
+public class Req003Specs {
+
+	@Steps
+	Req003Steps steps;
+
+	@Quando("^preencho com dados válidos o formulário$")
+	public void preencho_com_dados_válidos_o_formulário(DataTable tabela) throws Exception {
+		Map<String, String> dados = tabela.asMap(String.class, String.class);
+
+		steps.informar_nome(dados.get("nome"));
+		steps.informar_emais(dados.get("email"));
+		steps.informar_tipo_mensagem(dados.get("tipo"));
+		steps.informar_faixa_idade(dados.get("idade"));
+		steps.informar_mensagem(dados.get("msg"));
+	}
+
+	@Quando("^submeto o formulário$")
+	public void submeto_o_formulário() throws Exception {
+		steps.submeta();
+	}
+
+	@Então("^verifico que a mensagem de sucesso foi exibida$")
+	public void verifico_que_a_mensagem_de_sucesso_foi_exibida() throws Exception {
+		steps.verificar_mensagem_sucesso();
+	}
+
+}
