7b23d9968845454233d334cfd393b710269d9050
Diego Rustnnes Bandeira <diego.rustnnes@conductor.com.br>
2020-09-02 01:06:25 -0300

    arch: base from Professor's project, with some Class names edited and organized in packages

---
+++ src/test/java/br/unipe/alcool70/calculadoradedescontos/ui/pages/InicialPage.java
@@ -0,0 +1,45 @@
+package br.unipe.alcool70.calculadoradedescontos.ui.pages;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.FindBy;
+
+import static org.openqa.selenium.firefox.FirefoxDriverLogLevel.FATAL;
+
+public class InicialPage extends MyPageObject {
+
+	@FindBy(id = Locators.ID_BTNCALCULARDESCONTO)
+	WebElement botaoCalcularDesconto;
+	@FindBy(id = Locators.ID_BTNAJUDA)
+	WebElement botaoAjuda;
+	@FindBy(id = Locators.ID_BTNCONTATO)
+	WebElement botaoContato;
+
+	void acessarCalculoDesconto() {
+		clicar(botaoCalcularDesconto);
+	}
+
+	void acessarAjuda() {
+		clicar(botaoAjuda);
+	}
+
+	public void acessePaginaContato() {
+		clicar(botaoContato);
+	}
+
+	public void get(String url) {
+		WebDriverManager.firefoxdriver().setup();
+
+		FirefoxOptions opts = new FirefoxOptions().setLogLevel(FATAL);
+		opts.setCapability("marionette", true);
+
+		WebDriver drv = new FirefoxDriver(opts);
+		setDriver(drv);
+		getDriver().manage().window().maximize();
+		getDriver().get(url);
+	}
+
+}
---
+++ src/test/java/br/unipe/alcool70/calculadoradedescontos/ui/steps/SharedSteps.java
@@ -0,0 +1,19 @@
+package br.unipe.alcool70.calculadoradedescontos.ui.steps;
+
+import br.unipe.alcool70.calculadoradedescontos.ui.pages.InicialPage;
+import net.thucydides.core.annotations.Step;
+
+public class SharedSteps {
+	// Comunica com o PageObject
+	InicialPage page;
+
+	@Step
+	public void acesse_pagina_inicial() {
+		page.get("https://calculadora.diegoquirino.net/");
+	}
+
+	@Step
+	public void acesse_pagina_contato() {
+		page.acessePaginaContato();
+	}
+}
